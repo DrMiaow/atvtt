ed7d896cc5a687bebf4c721c2de147341bed82ee
James McParlane <james@massive.co>
2021-03-26 22:16:39 +0000

    Add basic server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const WebSocket = require('ws');
 var raspividStream = require('raspivid-stream');
 
 var videoStream = raspividStream();
@@ -6,5 +7,39 @@ var videoStream = raspividStream();
 videoStream.on('data', (data) => {
 	console.log('got data ' + data.length)
 	console.log(data.toString('hex'))
-//ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+
+	for (const ws of connections) {
+		ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });	
+	}	
 });
+
+
+const PORT = 8123;
+
+const connections = []
+
+async function startServer() {
+	wss = new WebSocket.Server({ port: PORT });
+	console.log('Server ready on port ' + PORT);
+	wss.on('connection', (ws) => {
+		console.log('Socket connected. sending data...');
+		
+		connections.push(ws);
+
+		ws.on('error', (error) => {
+			console.log('WebSocket error');
+		});
+		ws.on('close', (msg) => {
+			console.log('WebSocket close');
+
+			// Remove connection from array.
+			const index = connections.indexOf(ws);
+			if (index > -1) {				
+				connections.splice(index, 1);
+			}			
+
+		});
+	});
+}
+
+startServer();
